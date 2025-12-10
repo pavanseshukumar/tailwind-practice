d2c45dc3a32cacfa4687c7d951be3d54156bbad3
seshu31 <pavan.seshu@matrixintel.com>
2025-12-10 16:40:52 +0530

    practiced few aceternity components

--- app/page.js
+++ app/page.js
@@ -1,65 +1,104 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { EncryptedText } from "@/components/ui/encrypted-text";
+import { NoiseBackground } from "@/components/ui/noise-background";
+import { PixelatedCanvas } from "@/components/ui/pixelated-canvas";
+import { cn } from "@/lib/utils";
+import React from "react";
 
-export default function Home() {
+const page = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="max-w-4xl mx-auto mt-10">
+      <h1 className="text-2xl font-bold mb-5">Bento Grid</h1>
+      <Button
+        variant="primary"
+        className="bg-blue-500 text-white cursor-pointer"
+      >
+        Click me
+      </Button>
+      <p className="text-left">
+        You are not your job, you&apos;re not how much money you have in the
+        bank. You are not the car you drive. You&apos;re not the contents of
+        your wallet. You are not your fucking khakis.{" "}
+        <EncryptedText text="All singing, all dancing crap of the world." />
+      </p>
+
+      <div className="mx-auto max-w-sm">
+        <NoiseBackground
+          gradientColors={[
+            "rgb(255, 100, 150)",
+            "rgb(100, 150, 255)",
+            "rgb(255, 200, 100)",
+          ]}
+        >
+          <Card>
+            <div className="px-4 py-2">
+              <EncryptedText text="How to create a bento grid with Tailwind" />
+              <h3 className="text-left text-lg font-semibold text-balance text-neutral-800 dark:text-neutral-200"></h3>
+              <p className="mt-2 text-left text-sm text-neutral-600 dark:text-neutral-400">
+                Learn how to create a bento grid with Tailwind CSS, Next.js and
+                Framer Motion.
+              </p>
+            </div>
+          </Card>
+        </NoiseBackground>
+      </div>
     </div>
   );
-}
+};
+
+// const page = () => {
+//   return (
+//     <div className="mx-auto mt-8 flex w-full items-center justify-center">
+//       <PixelatedCanvas
+//         src={"./main.jpeg"}
+//         width={800}
+//         height={600}
+//         cellSize={4}
+//         dotScale={0.9}
+//         shape={"circle"}
+//         backgroundColor="#000000"
+//         dropoutStrength={0.1}
+//         interactive
+//         distortionStrength={0.1}
+//         distortionRadius={200}
+//         distortionMode="repel"
+//         followSpeed={0.2}
+//         jitterStrength={4}
+//         jitterSpeed={1}
+//         sampleAverage
+//         className="rounded-xl shadow-lg"
+//       />
+//     </div>
+//   );
+// };
+
+export default page;
+
+const Card = ({ className, children }) => {
+  return (
+    <div
+      className={cn(
+        "flex h-full min-h-80 flex-col overflow-hidden rounded-lg bg-white text-center dark:bg-neutral-800",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+const CardHeader = ({ children }) => {
+  return <div className="flex gap-2">{children}</div>;
+};
+
+const CardBody = ({ children }) => {
+  return <div className="flex ">{children}</div>;
+};
+
+const CardSkeleton = ({ children }) => {
+  return (
+    <div className="flex bg-neutral-400/50 rounded-md p-2 mt-4 overflow-hidden bg-[radial-gradient(var(--color-neutral-700)_1px,transparent_1px)] bg-size-[10px_10px] mask-radial-from-40%">
+      {children}
+    </div>
+  );
+};
